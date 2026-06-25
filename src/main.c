8c2597a9b555b13562992d1cebf610ae3fe31bf0
YerdosNar <besway2000@gmail.com>
2026-06-25 11:30:40 +0900

    refactor: Divided into separate line-related functions

--- src/main.c
+++ src/main.c
@@ -1,30 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+char *read_line(void)
+{
+        char *input= NULL;
+        size_t bufsize = 0;
+
+        if (getline(&input, &bufsize, stdin) == -1)
+                exit(EXIT_FAILURE);
+
+        return input;
+}
+
+char **tokenizer(char *line)
+{
+        char **cmds = malloc(64 * sizeof(char*));
+        if (!cmds) exit(EXIT_FAILURE);
+        int i = 0;
+        cmds[i] = strtok(line, " \n\t");
+        while (cmds[i] != NULL && i < 64)
+                cmds[++i] = strtok(NULL, " \n\t");
+
+        return cmds;
+}
+
 int shell_loop()
 {
-        char input[1024];
-        printf("> ");
-        fgets(input, sizeof(input), stdin);
-        char *cmds[64];
-        int i = 0;
-        cmds[i] = strtok(input, " \n\t");
-        while(cmds[i] != NULL) cmds[++i] = strtok(NULL, " \n\t");
+        while (1) {
+                printf("> ");
+                char *input = read_line();
+                char **cmds = tokenizer(input);
 
-        pid_t pid = fork();
-        if (0 == pid) {
-                if (execvp(cmds[0], cmds) == -1) return -1;
-        } else if (0 < pid) wait(NULL);
+                pid_t pid = fork();
+                if (0 == pid) {
+                        if (execvp(cmds[0], cmds) == -1) return -1;
+                } else if (0 < pid) wait(NULL);
 
+                free(input);
+                free(cmds);
+        }
         return 0;
 }
 
 int main(void)
 {
-        while (1) shell_loop();
+        shell_loop();
 
         return 0;
 }
