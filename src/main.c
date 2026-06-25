ddd21e41c2f54271a4392a462eb3f9b5163f0ea1
YerdosNar <besway2000@gmail.com>
2026-06-25 10:48:59 +0900

    feat: Working shell with infinite loop

--- src/main.c
+++ src/main.c
@@ -4,22 +4,27 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int main(void)
+int shell_loop()
 {
         char input[1024];
         printf("> ");
         fgets(input, sizeof(input), stdin);
         char *cmds[64];
         int i = 0;
-        cmds[i] = strtok(input, " \n");
-        while (cmds[i] != NULL) cmds[++i] = strtok(NULL, " \n");
+        cmds[i] = strtok(input, " \n\t");
+        while(cmds[i] != NULL) cmds[++i] = strtok(NULL, " \n\t");
 
         pid_t pid = fork();
-        if (pid == 0) {
+        if (0 == pid) {
                 if (execvp(cmds[0], cmds) == -1) return -1;
-        } else if (pid > 0) {
-                wait(NULL);
-        }
+        } else if (0 < pid) wait(NULL);
+
+        return 0;
+}
+
+int main(void)
+{
+        while (1) shell_loop();
 
         return 0;
 }
